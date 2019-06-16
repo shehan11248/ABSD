eeb26c944a262fbdcd2e6ffa3b7cedcda16a34cc
shehan <snimeshaka@gmail.com>
2019-06-16 09:15:54 +0530

    create Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ManageCustomersComponent} from './view/manage-customers/manage-customers.component';
+import {ManageItemComponent} from './view/manage-item/manage-item.component';
+import {DashboardComponent} from './view/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ManageCustomersComponent,
+    ManageItemComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
